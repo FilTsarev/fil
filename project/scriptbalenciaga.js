de9c71b4be8343b3614fba2134f0a4d9cb881aab
FilTsarev <106603612+FilTsarev@users.noreply.github.com>
2022-06-03 18:28:53 +0300

    Add files via upload

---
+++ project/scriptbalenciaga.js
@@ -0,0 +1,7 @@
+function addtobusketbalenciaga() {
+    sizebalenciaga = document.getElementById("sizebalenciaga").value;
+    console.log(sizebalenciaga);
+    
+}
+document.querySelector(".balenciaga").addEventListener("click", addtobusketbalenciaga);
+document.getElementById("balenciagacheck").style.display = 'none'; // hide
