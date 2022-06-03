a1ca7e21bb0a1d64ee4156c76ee6f222604a1fe2
FilTsarev <106603612+FilTsarev@users.noreply.github.com>
2022-06-03 18:29:20 +0300

    Add files via upload

---
+++ project/scriptoffwhite.js
@@ -0,0 +1,6 @@
+function addtobusketoffwhite() {
+    sizeoffwhite = document.getElementById("sizeoffwhite").value;
+    console.log(sizeoffwhite);
+    alert(sizeoffwhite);
+}
+document.querySelector(".offwhite").addEventListener("click", addtobusketoffwhite);
