e47901ee504a15d26a5d4b27b80273e8f5862e03
FilTsarev <106603612+FilTsarev@users.noreply.github.com>
2022-06-03 18:29:06 +0300

    Add files via upload

---
+++ project/scriptnike.js
@@ -0,0 +1,6 @@
+function addtobusketnike() {
+    sizenike = document.getElementById("sizenike").value;
+    console.log(sizenike);
+    alert(sizenike);
+}
+document.querySelector(".nike").addEventListener("click", addtobusketnike);
